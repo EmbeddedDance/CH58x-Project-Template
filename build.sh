842890bcfe494c8887ed861e59fa27e4ebaebc06
李大冶 <42072641+lz00qs@users.noreply.github.com>
2024-04-18 17:55:54 +0800

    feat: add compile_commands.json generation option to build script

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@
 if [ -d "build/" ]; then
     echo "Found build/"
     cd build
-    cmake -GNinja ..
+    cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -GNinja ..
     ninja
 else
     echo "Not Found build/"
     mkdir build
     cd build
-    cmake -GNinja ..
+    cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -GNinja ..
     ninja
 fi
